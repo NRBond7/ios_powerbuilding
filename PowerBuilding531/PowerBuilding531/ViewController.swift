7db2697ab951a54ffba0a7d4146ffe299820e65e
Bond, Noah <Noah.Bond@tdameritrade.com>
2018-10-05 10:51:24 -0400

    Created launch screen

---
+++ PowerBuilding531/PowerBuilding531/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PowerBuilding531
+//
+//  Created by Bond, Noah on 10/5/18.
+//  Copyright © 2018 Bond, Noah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
